3f8d6ac9853cc29d6e1fc0111740abe7b8ff47e4
romane-ledru <romane.ledru@etu.univ-lyon1.fr>
2023-03-15 17:23:16 +0100

    [BO] 2.1. Serveur de fichiers statiqus

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import path from 'path';
+
+import { sendResponse } from './handlers/error/handle-error-response';
 
 import { serverPort } from './config/env.config';
 import auth from './route/auth';
@@ -6,12 +9,22 @@ import auth from './route/auth';
 const app = express();
 const port = serverPort;
 
+const dirname = path.resolve();
+const options = {
+  root: path.join(dirname, 'public'),
+};
+
 app.use(express.json()); // Permet de parser le body des requêtes.
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', auth);
+app.use('/static', express.static('public'));
+app.get('/', (_req, res) => {
+  res.status(200).sendFile('/index.html', options);
+});
 
-app.get('/', (_, res) => {
-  res.send('Express + TypeScript Server');
+// Add 404 handler.
+app.use((req, res) => {
+  sendResponse(req, res, 404, true, undefined, options);
 });
 
 app.listen(port, () => {
