2a0445fe42d8f1e4bc8587fff2665eb904ee9c5c
MEDINI LIONEL <lionel.medini@univ-lyon1.fr>
2023-04-11 23:28:28 +0000

    Upload New File

---
+++ tp5/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form id="loginForm">
+    <label for="login">Login :</label>
+    <input type="text" name="login" id="login" />
+    <br />
+    <label for="password">Password :</label>
+    <input type="password" name="password" id="password" />
+    <br />
+    <button v-on:click="login">Send</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  methods: {
+    login: () => {
+      console.log("Login cliqué.");
+      //TODO qqch ici
+    },
+  },
+};
+</script>
+
+<style scoped>
+input,
+input[type="submit"],
+select {
+  background-color: #2f4f4f !important;
+  color: lightgray;
+  border: 1px solid;
+}
+</style>
