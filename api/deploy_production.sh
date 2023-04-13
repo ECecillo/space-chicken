e024f26508b8fbc87d46de0122ec8b1fbaba24f1
Enzo CECILLON <enzocecillon@MacBook-Pro-de-Enzo.local>
2023-04-13 20:24:09 +0200

    ci: extract ci logic to shell script to be more readable

---
+++ api/deploy_production.sh
@@ -0,0 +1,12 @@
+echo "EXPRESS_PORT=$EXPRESS_PORT" > .env \
+  &&  echo "NODE_ENV=$NODE_ENV" >> .env \
+  &&  echo "LOCAL=http://localhost:8080" >> .env \
+  &&  echo "SPRING_SERVER=$SPRING_SERVER" >> .env \
+  &&  cat .env \
+  &&  npm install --include=dev \
+  &&  npm run build \
+  &&  npm prune --production \
+  &&  npm ci \
+  &&  mv .env public build \
+  &&  scp -r -o StrictHostKeyChecking=no -i $CI_SSH_KEY build node_modules gitlabci@192.168.75.14:/home/gitlabci \
+  &&  ssh -i $CI_SSH_KEY gitlabci@192.168.75.14 "pm2 restart server"
