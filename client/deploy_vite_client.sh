3d7252d842eb23607f35355117cb89182e2a2195
Enzo CECILLON <enzocecillon@MacBook-Pro-de-Enzo.local>
2023-05-24 18:34:53 +0200

    fix: client deploy script not using env var for building static files

--- client/deploy_vite_client.sh
+++ client/deploy_vite_client.sh
@@ -1,3 +1,6 @@
-npm install --include=dev \
-&&  npm run build \
-&&  scp -r -o StrictHostKeyChecking=no -i $CI_SSH_KEY dist/* gitlabci@192.168.75.14:/var/www/html/ \
+echo "VITE_NODE_ENV=$NODE_ENV" > .env \
+ && echo "VITE_CLIENT_AUTHENTICATION_API=$AUTH_PATH">> .env \
+ && echo "VITE_CLIENT_RESOURCES_API=$API_PATH">> .env \
+ && npm install --include=dev \
+ &&  npm run build \
+ &&  scp -r -o StrictHostKeyChecking=no -i $CI_SSH_KEY dist/* gitlabci@192.168.75.14:/var/www/html/ \
