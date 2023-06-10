e68e63e0d32ab62316faf7feddcae3bf29b94897
LEDRU ROMANE p2105081 <romane.ledru@etu.univ-lyon1.fr>
2023-06-10 14:28:33 +0000

    Update file deploy_production.sh

--- api/deploy_production.sh
+++ api/deploy_production.sh
@@ -4,8 +4,7 @@ echo "EXPRESS_PORT=$EXPRESS_PORT" > .env \
   &&  cat .env \
   &&  npm install --include=dev \
   &&  npm run build \
-  &&  npm prune --production \
-  &&  npm ci \
-  &&  mv .env build \
-  &&  scp -r -o StrictHostKeyChecking=no -i $CI_SSH_KEY public build node_modules gitlabci@192.168.75.14:/home/gitlabci \
-  &&  ssh -i $CI_SSH_KEY gitlabci@192.168.75.14 "pm2 restart server"
+  &&  cp .env build/.env \
+  &&  scp -r -o StrictHostKeyChecking=no -i $CI_SSH_KEY public build .env package.json package-lock.json gitlabci@192.168.75.14:/home/gitlabci \
+  &&  ssh -i $CI_SSH_KEY gitlabci@192.168.75.14 "npm install --omit=dev" \
+  &&  ssh -i $CI_SSH_KEY gitlabci@192.168.75.14 "pm2 start build/server.js --name server || pm2 restart server"
